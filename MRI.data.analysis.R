7ce8ed6cf489e72cff07e33eba473a60c7bca4cb
gcuijpers <germen.cuijpers@gmail.com>
2023-11-03 16:10:17 +0100

    Update MRI.data.analysis.R

--- MRI.data.analysis.R
+++ MRI.data.analysis.R
@@ -1,4 +1,6 @@
 library(readr)
+library(moments)
+
 ## importing data 
 df <- read_csv("Desktop/computational research skills/MRI-scheduling/ScanRecords.csv")
 
@@ -12,5 +14,13 @@ Dur1_mean <- mean(df_type1$Duration)
 Dur1_sd <- sd(df_type1$Duration)
 h1 <- hist(df_type1$Duration)
 
-## Distribution of durtion of Type 2 is unknown
+## Distribution of duration of Type 2 is unknown
+## Try normal distribution 
 h2 <- hist(df_type2$Duration)
+skewness_dur2 <- skewness(df_type2$Duration)
+kurtosis_dur2 <- kurtosis(df_type2$Duration)
+jarque.test(df_type2$Duration)
+ks.test(df_type1$Duration, "pnorm")
+ks.test(df_type1$Duration, df_type2$Duration)
+
+
