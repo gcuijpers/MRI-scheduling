dbba1bc81a91e5ce6492dd744d5767fb460253f2
gcuijpers <germen.cuijpers@gmail.com>
2023-11-03 14:25:39 +0100

    Update MRI.data.analysis.R

--- MRI.data.analysis.R
+++ MRI.data.analysis.R
@@ -6,3 +6,11 @@ df <- read_csv("Desktop/computational research skills/MRI-scheduling/ScanRecords
 df_type1 <- df[df$PatientType == 'Type 1',]
 df_type2 <- df[df$PatientType == 'Type 2',]
 
+## Duration op Type 1 patients is normally distributed (known information)
+## Mean and sd can easily be calculated the following 
+Dur1_mean <- mean(df_type1$Duration)
+Dur1_sd <- sd(df_type1$Duration)
+h1 <- hist(df_type1$Duration)
+
+## Distribution of durtion of Type 2 is unknown
+h2 <- hist(df_type2$Duration)
