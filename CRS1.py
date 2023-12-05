c0ac0519c4f4ab1935fb2f3ba7b6ae097e2ab739
Stivvay <149711842+Stivvay@users.noreply.github.com>
2023-12-05 18:30:51 +0100

    Update CRS1.py

--- CRS1.py
+++ CRS1.py
@@ -5,25 +5,29 @@ Created on Wed Nov  1 15:30:36 2023
 
 import numpy as np
 
-lam_1 = 16.47826
-scan_mean_1 = 0.432
+#type 1
+lam_1 = 16.47826 # arrivals per day ~Poisson (inter-arrival ~Exp(9/lam))
+scan_mean_1 = 0.432 # scan duration ~N
 scan_sd_1 = 0.0974
 
-lam_2 = 10.3913
-scan_shape = 12.583
+#type 2
+ia_mean = 0.8666387 # interarrival times ~N
+ia_sd = 0.31
+scan_shape = 12.583 # scan duration ~Gamma
 scan_scale = 1/18.800
 
 n_slot_1 = 18
 n_slot_2 = 12
 
-days = 1000
+n_days = 10000
 
-def gen_one(lam, mean, sd):
+#generates list of type 1 patients and assigns randomly drawn scan duration as attribute of patient
+def gen_one(lam, mean, sd): 
     day = 0 
     time = 8
     patients = []
     
-    while day < days:
+    while day < n_days:
         arr_time = time + 9*np.random.exponential(1/lam)
         if arr_time > 17:
             day += 1 
@@ -35,29 +39,31 @@ def gen_one(lam, mean, sd):
     patients = patients[:-1]
     return patients   
 
-def gen_two(lam, mean, sd):
+#generates list of type 2 patients and assigns randomly drawn scan duration as attribute of patient
+def gen_two(ia_mean, ia_sd, scan_mean, scan_sd):
     day = 0 
     time = 8
     patients = []
     
-    while day < days:
-        arr_time = time + 9*np.random.exponential(1/lam)
+    while day < n_days:
+        arr_time = time + np.random.normal(ia_mean, ia_sd)
         if arr_time > 17:
             day += 1 
             arr_time -= 9    
-        scan_dur = np.random.gamma(mean,sd)
+        scan_dur = np.random.gamma(scan_mean,scan_sd)
         time = arr_time
-        patients.append([day,time,scan_dur])
+        patients.append([day,time,scan_dur,2])
     patients = np.array(patients)
     patients = patients[:-1]
     return patients   
 
+#schedule: returns list of day, time, patient index
 def schedule(patients, n_slot):
     schedule = []
     sch_day = 1
     slot = 0
     for pat in patients:
-        if pat[0] == sch_day or slot == n_slot:
+        if pat[0] == sch_day or slot == n_slot: # go to next day if no more available slots
             sch_day += 1 
             slot = 0
         patient_slot = [sch_day,8+slot*(9/n_slot)]
@@ -74,7 +80,7 @@ def perf_eval(schedule, patients):
     time = 8
 
     for i in range(len(schedule)):
-        lateness.append(max(0, time - schedule[i][1]))
+        lateness.append(max(0, time - schedule[i][1])) # if early, lateness = 0
         scan_sum += patients[i][2]
         time = max(time, schedule[i][1]) + patients[i][2]
         
@@ -90,24 +96,24 @@ def perf_eval(schedule, patients):
 
 
 pat_1 = gen_one(lam_1, scan_mean_1, scan_sd_1)
-pat_2 = gen_two(lam_2, scan_shape, scan_scale)
+pat_2 = gen_two(ia_mean, ia_sd, scan_shape, scan_scale)
 
 sch_1 = schedule(pat_1, n_slot_1)
 sch_2 = schedule(pat_2, n_slot_2)
 
+# day of scheduling - day of calling
 days_wait_type1 = [int(sch_day - call_day) for sch_day, call_day in zip(sch_1[:,0], pat_1[:,0])] 
 days_wait_type2 = [int(sch_day - call_day) for sch_day, call_day in zip(sch_2[:,0], pat_2[:,0])] 
 
 lateness_t1, overtime_t1, idletime_t1 = perf_eval(sch_1, pat_1)
 lateness_t2, overtime_t2, idletime_t2 = perf_eval(sch_2, pat_2)
 
-
-
 print("TYPE 1")
 print("Average lateness:", round(np.average(lateness_t1)*60,2), "minutes")
 print("lateness 25th quantile:", round(np.quantile(lateness_t1, 0.25)*60,2), "minutes")
 print("lateness 50th quantile:", round(np.quantile(lateness_t1, 0.5)*60,2), "minutes")
 print("lateness 75th quantile:", round(np.quantile(lateness_t1, 0.75)*60,2), "minutes")
+print("lateness 90th quantile:", round(np.quantile(lateness_t1, 0.9)*60,2), "minutes")
 print("Maximum lateness:", round(np.max(lateness_t1)*60,2), "minutes")
 print("Average days wait:", round(np.average(days_wait_type1),2), "days")
 print("Maximum days wait:", round(np.max(days_wait_type1),2), "days")
@@ -120,6 +126,7 @@ print("Average lateness:", round(np.average(lateness_t2)*60,2), "minutes")
 print("lateness 25th quantile:", round(np.quantile(lateness_t2, 0.25)*60,2), "minutes")
 print("lateness 50th quantile:", round(np.quantile(lateness_t2, 0.5)*60,2), "minutes")
 print("lateness 75th quantile:", round(np.quantile(lateness_t2, 0.75)*60,2), "minutes")
+print("lateness 90th quantile:", round(np.quantile(lateness_t2, 0.9)*60,2), "minutes")
 print("Maximum lateness:", round(np.max(lateness_t2)*60,2), "minutes")
 print("Average days wait:", round(np.average(days_wait_type2),2), "days")
 print("Maximum days wait:", round(np.max(days_wait_type2),2), "days")
